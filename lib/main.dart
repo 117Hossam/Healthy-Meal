e1c06c2762af04788713787bccb464b78975bdee
Hossam Abu Dawood <12333576@students.liu.edu.lb>
2025-11-24 20:23:37 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'meals_data.dart';
+import 'meal.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Healthy Meal',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String selectedCategory = "Breakfast"; // default
+  Meal? selectedMeal; // chosen meal
+
+  @override
+  Widget build(BuildContext context) {
+    final categoryMeals = mealsByCategory[selectedCategory]!;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Healthy Meal",style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.bold),), backgroundColor: Colors.greenAccent,centerTitle: true,),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text("Select Category", style: TextStyle(fontSize: 22,color: Colors.deepPurple,fontWeight: FontWeight.w600)),
+            const SizedBox(height: 10),
+
+            // DropdownMenu for categories
+            DropdownMenu(
+              initialSelection: selectedCategory,
+              onSelected: (mealCategory) {
+                setState(() {
+                  selectedCategory = mealCategory!;
+                  selectedMeal = null; // reset meal when category changes
+                });
+              },
+              dropdownMenuEntries: mealsByCategory.keys.map((category) {
+                return DropdownMenuEntry(value: category, label: category);
+              }).toList(),
+            ),
+
+            const SizedBox(height: 20),
+            const Text("Select Meal", style: TextStyle(fontSize: 22,color: Colors.deepPurple, fontWeight: FontWeight.w600)),
+            const SizedBox(height: 10),
+
+            // Radio buttons for meals
+            Column(
+              children: categoryMeals.map((meal) {
+                return RadioListTile<Meal>(
+                  title: Text(meal.name,style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.w600),),
+
+                  subtitle: Text(meal.description),
+                  value: meal,
+                  groupValue: selectedMeal,
+                  onChanged: (Meal? value) {
+                    setState(() {
+                      selectedMeal = value;
+                    });
+                  },
+                );
+              }).toList(),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Meal details
+            if (selectedMeal != null) SingleChildScrollView(
+                child: Card(
+                  color: Colors.greenAccent,
+                  margin: const EdgeInsets.all(15),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(selectedMeal!.name,
+                            style: const TextStyle(
+                                color: Colors.deepPurple,fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(selectedMeal!.description,),
+                            // style: const TextStyle(color: Colors.grey)),
+                        const Divider(),
+                        const Text("Ingredients:",
+                            style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.w600)),
+                        ...selectedMeal!.ingredients
+                            .map((ingredients) => Text("• $ingredients")),
+                        const Divider(),
+                        const Text("Steps:",
+                            style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.w600)),
+                        ...selectedMeal!.steps.asMap().entries.map(
+                                (entries) => Text("${entries.key + 1}. ${entries.value}")),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/meal.dart
@@ -0,0 +1,19 @@
+class Meal {
+  String _name;
+  String _description;
+  List<String> _ingredients;
+  List<String> _steps;
+
+  Meal(this._name, this._description, this._ingredients, this._steps);
+
+  String get name => _name;
+  String get description => _description;
+  List<String> get ingredients => _ingredients;
+  List<String> get steps => _steps;
+
+
+  @override
+  String toString() {
+    return _name; // used in radio button labels
+  }
+}
